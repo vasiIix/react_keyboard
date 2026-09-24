b6e7eb3a0ff4c2b54cc0beb9aea45ca3781916e0
vasylyx <mr.vasilishin@gmail.com>
2026-09-24 11:11:22 +0300

    add componentWillUnmount

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,16 @@ export class App extends React.Component<Props, State> {
     pressedKey: 'Nothing was pressed yet',
   };
 
+  handleKeyUp = (event: KeyboardEvent): void => {
+    this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
+  };
+
   componentDidMount(): void {
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-      this.setState({ pressedKey: `The last pressed key is [${event.key}]` });
-    });
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
 
   render(): React.ReactNode {
